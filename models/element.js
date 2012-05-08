dcd2acc7eccc1adf2f0ad39de9d4b84655bda3a5
devote <spb.piksel@gmail.com>
2012-05-08 07:09:52 +0400

    update Element model

--- models/element.js
+++ models/element.js
@@ -1,5 +1,5 @@
 /*
- * element.js DOM Element Model v0.0.3 for Internet Explorer < 8
+ * element.js DOM Element Model v0.1.0 for Internet Explorer < 8
  *
  * Copyright 2012, Dmitriy Pakhtinov ( spb.piksel@gmail.com )
  *
@@ -9,7 +9,7 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Update: 24-04-2012
+ * Update: 08-05-2012
  */
 
 (function( window, undefined ) {
@@ -18,80 +18,90 @@
 
 	var document = window.document;
 
+	// if not exists Element interface
 	if ( !window.Element && document.attachEvent ) {
 
 		var ready = false,
+			// save originals methods
 			__getElementById = document.getElementById,
 			__createElement = document.createElement,
 			__createDocumentFragment = document.createDocumentFragment,
+			__attachEvent = document.attachEvent,
+			// create dynamic fragment for intercept elements
 			fragment = __createDocumentFragment(),
-			prototype = {},
-			Element = function(){}
+			// for store the method names
+			prototypeNames = [],
+			// for store index on method names
+			prototypeIndexes = {},
+			// interface Element
+			Element = function(){},
+			proto,
 
-		Element.prototype = document.createComment( "" );
-		Element.prototype.attachEvent( 'onpropertychange', function() {
+		// intercept change element content
+		propChange = function() {
+			if ( window.event.propertyName === "innerHTML" ) {
+				addMethods( window.event.srcElement );
+			}
+		},
 
-			var name = window.event.propertyName;
+		// set methods from prototype Element to new/all element
+		addMethods = function( elems, methodName ) {
 
-			prototype[ name ] = 1;
+			var i = prototypeNames.length,
+				l, idx, name, elem;
 
-			var elems = document.getElementsByTagName( '*' );
-			for( var l = elems.length; elem = elems[ --l ]; ) {
-				if ( elem.nodeType === 1 ) {
-					elem[ name ] = Element.prototype[ name ];
-				}
+			if ( elems && elems.nodeType || !elems ) {
+				elems = ( elems || document ).getElementsByTagName( '*' );
 			}
 
-			elems = fragment.getElementsByTagName( '*' );
-			for( var l = elems.length; elem = elems[ --l ]; ) {
+			for( l = elems.length; elem = elems[ --l ]; ) {
 				if ( elem.nodeType === 1 ) {
-					elem[ name ] = Element.prototype[ name ];
-				}
-			}
-		});
-		document.documentElement.firstChild.appendChild( Element.prototype );
-
-		window.Element = Element;
-
-		var copyMethod = function( elem ) {
-			if ( elem && elem.nodeType === 1 ) {
-				for( var key in prototype ) {
-					if ( Object.prototype.hasOwnProperty.call( prototype, key ) ) {
-						if ( elem[ key ] !== Element.prototype[ key ] ) {
-							elem[ key ] = Element.prototype[ key ];
+					if ( methodName ) {
+						if ( elem[ methodName ] !== proto[ methodName ] ) {
+							elem[ methodName ] = proto[ methodName ];
+						}
+					} else {
+						for( idx = i; name = prototypeNames[ --idx ]; ) {
+							if ( elem[ name ] !== proto[ name ] ) {
+								elem[ name ] = proto[ name ];
+							}
+						}
+						if ( !elem.__propChangeAttached ) {
+							elem.__propChangeAttached = 1;
+							elem.attachEvent( "onpropertychange", propChange );
 						}
 					}
 				}
-				if ( !elem.__propChangeAttached ) {
-					elem.__propChangeAttached = 1;
-					elem.attachEvent( "onpropertychange", propChange );
-				}
 			}
-			return elem;
+
+			return elems[ 0 ] || null;
 		}
 
-		var propChange = function() {
-			if ( window.event.propertyName === "innerHTML" ) {
-				var elems = copyMethod( window.event.srcElement ).getElementsByTagName( '*' );
-				for( var l = elems.length; copyMethod( elems[ --l ] ); ) {}
-			}
-		}
+		Element.prototype = proto = document.createComment( "" );
 
-		var ie7setMethods = function() {
-			if ( ready ) {
-				return;
+		proto.attachEvent( 'onpropertychange', function() {
+
+			var name = window.event.propertyName;
+
+			if ( prototypeIndexes[ name ] === undefined ) {
+				prototypeIndexes[ name ] = prototypeNames.push( name ) - 1;
 			}
-			ready = true;
-			var elems = document.getElementsByTagName( '*' );
-			for( var l = elems.length; copyMethod( elems[ --l ] ); ) {}
-		}
+
+			addMethods( document, name );
+			addMethods( fragment, name );
+
+		});
+
+		document.documentElement.firstChild.appendChild( proto );
 
 		document.getElementById = function( id ) {
-			return copyMethod( __getElementById( id ) );
+			return addMethods( [ __getElementById( id ) ] );
 		}
 
 		document.createElement = function( tagName ) {
-			var elem = copyMethod( __createElement( tagName ) );
+			var elem = addMethods( [ __createElement( tagName ) ] );
+			// if element "INPUT" attach on the fragment or DOM object,
+			// Internet Explorer generate error if try change attribute "type".
 			if ( elem.nodeName !== "INPUT" ) {
 				fragment.appendChild( elem );
 			}
@@ -99,37 +109,38 @@
 		}
 
 		document.createDocumentFragment = function() {
-			return copyMethod( __createDocumentFragment() );
+			return addMethods( [  __createDocumentFragment() ] );
 		}
 
 		if ( document.readyState === "complete" ) {
 
-			ie7setMethods();
+			addMethods();
 		} else {
 
-			var __attachEvent = document.attachEvent,
-				readystate = function(){
-					if ( document.readyState === "complete" ) {
-						document.detachEvent( "onreadystatechange", readystate );
-						ie7setMethods();
-						document.attachEvent = __attachEvent;
-					}
+			var DOMContentLoaded = function() {
+				if ( !ready && document.readyState === "complete" ) {
+					ready = true;
+					document.detachEvent( "onreadystatechange", DOMContentLoaded );
+					document.attachEvent = __attachEvent;
+					addMethods();
 				}
+			}
+
+			__attachEvent( "onreadystatechange", DOMContentLoaded );
+			window.attachEvent( "onload", DOMContentLoaded );
 
 			document.attachEvent = function( event, listener ) {
 				if ( "onreadystatechange" === event ) {
-					document.detachEvent( event, readystate );
+					document.detachEvent( event, DOMContentLoaded );
 					var result = __attachEvent( event, listener );
-					__attachEvent( event, readystate );
+					__attachEvent( event, DOMContentLoaded );
 					return result;
 				}
 				return __attachEvent( event, listener );
 			}
-
-			__attachEvent( "onreadystatechange", readystate );
-
-			window.attachEvent( "onload", ie7setMethods );
 		}
+
+		window.Element = Element;
 	}
 
 })( window );
